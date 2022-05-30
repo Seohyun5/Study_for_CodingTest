e5743439d4beaa2a29947e529678efc6af928e90
Seohyun5 <80747110+Seohyun5@users.noreply.github.com>
2022-05-30 17:21:31 +0900

    Update 8958_OX퀴즈.java

--- Baekjoon/8958_OX퀴즈.java
+++ Baekjoon/8958_OX퀴즈.java
@@ -22,55 +22,55 @@ public class Main {
             sum = 0;
             idx = 0;
         }
-	  }
+    }
 }
 
 
 /* =====처음 풀이(168ms)=====
 public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = Integer.parseInt(br.readLine());
-		int sum = 0;
-		int idx = 0;
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	int n = Integer.parseInt(br.readLine());
+	int sum = 0;
+	int idx = 0;
 		
-		for(int i=0; i<n; i++) {
-        String[] arr = br.readLine().split("");
-        for(int j=0; j<arr.length; j++) {
-            if(arr[j].equals("O")) {
-                idx++;
-                sum += idx;
-            }else idx = 0;
-        }
-        bw.write(sum + "\n");
-        sum = 0;
-        idx = 0;
-		}
-		bw.flush();
-		bw.close();
+	for(int i=0; i<n; i++) {
+            String[] arr = br.readLine().split("");
+            for(int j=0; j<arr.length; j++) {
+            	if(arr[j].equals("O")) {
+                    idx++;
+                    sum += idx;
+            	}else idx = 0;
+            }
+            bw.write(sum + "\n");
+            sum = 0;
+            idx = 0;
+	}
+	bw.flush();
+	bw.close();
 }
 */
 
 
 /* =====두 번 째 풀이(152ms)=====
 public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int sum = 0;
-		int idx = 0;
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	int n = Integer.parseInt(br.readLine());
+	int sum = 0;
+	int idx = 0;
 		
-		for(int i=0; i<n; i++) {
-        String[] arr = br.readLine().split("");
-        for(int j=0; j<arr.length; j++) {
-            if(arr[j].equals("O")) {
-                idx++;
-                sum += idx;
-            }else idx = 0;
-        }
-        System.out.println(sum);
-        sum = 0;
-        idx = 0;
-		}
+	for(int i=0; i<n; i++) {
+            String[] arr = br.readLine().split("");
+            for(int j=0; j<arr.length; j++) {
+                if(arr[j].equals("O")) {
+                    idx++;
+                    sum += idx;
+                }else idx = 0;
+            }
+            System.out.println(sum);
+            sum = 0;
+            idx = 0;
+	}
 }
 
 // BufferedWriter를 System.out.println으로 바꿨고 속도가 빨라졌지만 여전히 불만족스러웠다.
