e6fc432514752570b8512cb7863d3f0450281df6
Seohyun5 <80747110+Seohyun5@users.noreply.github.com>
2022-06-18 23:59:35 +0900

    Create 10870_피보나치_수_5.java

---
+++ Baekjoon/10870_피보나치_수_5.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(Fibonacci(n));
+    }
+	
+    public static int Fibonacci(int n) {
+        if(n==0) return 0;
+        else if(n==1) return 1;
+        return Fibonacci(n-2) + Fibonacci(n-1);
+    }	
+}
