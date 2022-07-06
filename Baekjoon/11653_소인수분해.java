38ee7a30785a87eab2f1d661f7a45c499d8274ce
Seohyun5 <80747110+Seohyun5@users.noreply.github.com>
2022-07-06 16:10:54 +0900

    Create 11653_소인수분해.java

---
+++ Baekjoon/11653_소인수분해.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int i = 2;
+        while(n!=1) {
+            if(n%i==0) {
+                System.out.println(i);
+                n /= i;
+            }else {
+                i++;
+            }
+        }
+    }
+}
